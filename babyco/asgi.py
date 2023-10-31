788cab1873ead70a1958fcc913156b479915f0c1
Barissa-Imran <imranbarissa@gmail.com>
2023-10-31 03:31:25 +0300

    add app vercel

--- babyco/asgi.py
+++ babyco/asgi.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 
 import os
 
-from django.core.asgi import get_asgi_application
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'babyco.settings')
 
-application = get_asgi_application()
+app = get_wsgi_application()
