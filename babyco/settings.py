f0ac8f1702f3d7a3aca5396d27841ccb62721e2f
Barissa-Imran <imranbarissa@gmail.com>
2023-10-31 03:48:22 +0300

    add app vercel

--- babyco/settings.py
+++ babyco/settings.py
@@ -27,11 +27,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS = ['https://c965-102-68-77-46.ngrok-free.app']
+CSRF_TRUSTED_ORIGINS = ['*']
 
 
 # Application definition
